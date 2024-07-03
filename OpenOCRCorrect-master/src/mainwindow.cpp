a9254658c605207bf9f85396892f58360db79f67
HarshitM567 <harshit.mittal2021@vitbhopal.ac.in>
2024-07-03 20:10:57 +0530

    Added bold/unbold, subscript, and superscript features

---
+++ OpenOCRCorrect-master/src/mainwindow.cpp
@@ -0,0 +1,25 @@
+// Add this to your MainWindow.cpp or relevant file
+void MainWindow::on_boldButton_clicked() {
+    QTextCharFormat format;
+    format.setFontWeight(ui->textEdit->fontWeight() == QFont::Bold ? QFont::Normal : QFont::Bold);
+    ui->textEdit->mergeCurrentCharFormat(format);
+}
+
+void MainWindow::on_superscriptButton_clicked() {
+    QTextCharFormat format;
+    format.setVerticalAlignment(QTextCharFormat::AlignSuperScript);
+    ui->textEdit->mergeCurrentCharFormat(format);
+}
+
+void MainWindow::on_subscriptButton_clicked() {
+    QTextCharFormat format;
+    format.setVerticalAlignment(QTextCharFormat::AlignSubScript);
+    ui->textEdit->mergeCurrentCharFormat(format);
+}
+
+//About Me Section
+void MainWindow::on_aboutButton_clicked() {
+    QMessageBox msgBox;
+    msgBox.setText("Name\nResume Details\n...");
+    msgBox.exec();
+}
---
+++ OpenOCRCorrect-master/src/mainwindow.h
@@ -0,0 +1,14 @@
+//
+// Created by mitta on 03-07-2024.
+//
+
+#ifndef SRC_MAINWINDOW_H
+#define SRC_MAINWINDOW_H
+
+
+class mainwindow {
+
+};
+
+
+#endif //SRC_MAINWINDOW_H
